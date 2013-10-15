9d951d832e9f8fb4d346bd80698d5ea8ccc18583
Tak Tran <contact@tutaktran.com>
2013-10-15 11:22:41 +0100

    Show redirect.

--- public/listen/js/main.js
+++ public/listen/js/main.js
@@ -8,6 +8,7 @@
       var url = soundTransfer.URL_MAPPINGS[message];
       console.log("url", url);
       if (url) {
+        $(".redirect").text(url);
         // Hack - just use root url
         window.location = '/' + url;
       }
